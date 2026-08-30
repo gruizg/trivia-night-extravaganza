4d7c7ee68e463952f394d68d1c808b3563878cc2
Geshlee <geshlee.ruiz@gmail.com>
2026-08-30 15:13:04 -0400

    Implement read methods and tests for ThemeService.java

--- server/src/test/java/learn/trivia/domain/ThemeServiceTest.java
+++ server/src/test/java/learn/trivia/domain/ThemeServiceTest.java
@@ -2,18 +2,90 @@ package learn.trivia.domain;
 
 import learn.trivia.data.doubles.QuestionRepositoryDouble;
 import learn.trivia.data.doubles.ThemeRepositoryDouble;
+import learn.trivia.models.Question;
+import learn.trivia.models.Theme;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static learn.trivia.TestDataHelpers.Models.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThemeServiceTest {
 
-    ThemeService service = new ThemeService(new ThemeRepositoryDouble(), new QuestionRepositoryDouble());
+    ThemeService service;
+
+    @BeforeEach
+    void setup() { service = new ThemeService(new ThemeRepositoryDouble(), new QuestionRepositoryDouble()); }
 
     //TODO: IMPLEMENT TESTS
 
-    @Test
-    void check() {
+    @Nested
+    class Read {
 
+        @Test
+        void shouldFindThemeById() {
+
+            Theme expected = makeTheme();
+
+            Theme actual = service.findThemeById(1);
+
+            assertNotNull(actual);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotFindThemeById() {
+
+            Theme actual = service.findThemeById(99);
+
+            assertNull(actual);
+        }
+
+        @Test
+        void shouldFindAllThemes() {
+            Theme expected = makeTheme();
+
+            List<Theme> actual = service.findAllThemes();
+
+            assertNotNull(actual);
+            assertFalse(actual.isEmpty());
+            assertEquals(expected, actual.get(0));
+            assertEquals(2, actual.size());
+        }
+
+        @Test
+        void shouldFindQuestionById() {
+
+            Question expected = makeQuestion();
+
+            Question actual = service.findQuestionById(1);
+
+            assertNotNull(actual);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotFindQuestionById() {
+
+            Question actual = service.findQuestionById(99);
+
+            assertNull(actual);
+        }
+
+        @Test
+        void shouldFindQuestionsByThemeId() {
+
+            Question expected = makeQuestion();
+
+            List<Question> actual = service.findQuestionsByThemeId(1);
+
+            assertNotNull(actual);
+            assertFalse(actual.isEmpty());
+            assertEquals(expected, actual.get(0));
+            assertEquals(2, actual.size());
+        }
     }
 }
