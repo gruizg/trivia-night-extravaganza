4ab460504b2c7ee8370fa28f35e44f1fa80c004f
Geshlee <geshlee.ruiz@gmail.com>
2026-08-31 01:10:02 -0400

    View themes

--- client/src/app/themes/_components/ThemeCard.js
+++ client/src/app/themes/_components/ThemeCard.js
@@ -1,33 +1,23 @@
 export default function ThemeCard({ theme }) {
+
 return (
     <div className="flex flex-col justify-between rounded-xl border border-gray-200 bg-white p-6 shadow-sm transition-all hover:shadow-md dark:border-gray-800 dark:bg-gray-900">
         <div>
             {/* Title and Author Header */}
             <div className="mb-3">
                 <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {/*{theme.themeTitle}*/}
+                    {theme.themeTitle}
                 </h3>
                 <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                    {/*by {author}*/}
+                    by {theme.user.username}
                 </p>
             </div>
 
             {/* Description */}
             <p className="text-sm leading-relaxed text-gray-600 dark:text-gray-300">
-                {/*{description}*/}
+                {theme.themeDescription}
             </p>
         </div>
     </div>
     )
 }
-// {
-//     "themeId": 1,
-//     "themeTitle": "General Knowledge & Trivia Night",
-//     "themeDescription": "A standard 6-round trivia game with halftime and final questions.",
-//     "user": {
-//     "userId": 1,
-//         "username": "user",
-//         "email": "email",
-//         "password": "hashed-password"
-// }
-// }
--- client/src/app/themes/page.js
+++ client/src/app/themes/page.js
@@ -1,13 +1,23 @@
 import ThemeCard from "@/app/themes/_components/ThemeCard";
 
 async function getThemes() {
-    const response = await fetch('http://localhost:8080/api/theme')
+    const response = await fetch('http://localhost:8080/api/theme');
+
+    if (!response.ok) throw new Error("Themes not found");
+    return response.json();
 }
-export default function Themes() {
-return (
+
+export default async function Themes() {
+    const themes = await getThemes();
+    return (
+
         <div className={"page-container"}>
             <h1>Themes</h1>
-            <ThemeCard />
+            <ul>
+                {themes.map((theme) => (
+                    <ThemeCard key={theme.themeId} theme={theme}></ThemeCard>
+                ))}
+            </ul>
         </div>
     )
 }
