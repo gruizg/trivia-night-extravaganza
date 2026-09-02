244c6da6827ef65c1404595f5da6a08bdacae244
Geshlee <geshlee.ruiz@gmail.com>
2026-09-02 15:24:45 -0400

    Updates sql test data

--- server/sql/test-queries.sql
+++ server/sql/test-queries.sql
@@ -28,3 +28,32 @@ from question q
          left join theme t on q.theme_id = t.theme_id
          left join user u on t.user_id = u.user_id
 where t.theme_id = 1;
+
+
+
+
+select t.team_id, t.team_token, t.team_number, t.team_name,
+       g.game_id, g.game_code, g.host_token, g.game_status, g.current_question_id,
+       q.question_id, q.question_category, q.question_prompt, q.question_answer, q.question_type, q.question_round, q.question_order,
+       th.theme_id, th.theme_title, th.theme_description,
+       u.user_id, u.username, u.email, u.password
+
+from team t
+left join game g on t.game_id = g.game_id
+left join theme th on g.theme_id = th.theme_id
+left join question q on g.current_question_id = q.question_id and th.theme_id = q.theme_id
+left join user u on th.user_id = u.user_id
+where t.team_id = ?;
+
+
+
+select g.game_id, g.game_code, g.host_token, g.game_status, g.current_round, g.current_question_id,
+       q.question_id, q.question_category, q.question_prompt, q.question_answer, q.question_type, q.question_round, q.question_order,
+       th.theme_id, th.theme_title, th.theme_description,
+       u.user_id, u.username, u.email, u.password
+
+from game g
+    left join theme th on g.theme_id = th.theme_id
+    left join question q on g.current_question_id = q.question_id and th.theme_id = q.theme_id
+    left join user u on th.user_id = u.user_id
+where g.game_id = ?;
--- server/sql/test-schema.sql
+++ server/sql/test-schema.sql
@@ -42,7 +42,7 @@ create table game (
                       game_code text not null,
                       host_token text not null,
                       game_status text,
-                      current_round int,
+                      current_round int null,
                       current_question_id int null,
                       theme_id int null,
                       constraint fk_game_theme
@@ -59,16 +59,19 @@ create table game (
 );
 
 create table team (
-                      team_id int primary key auto_increment,
-                      team_token text not null,
-                      team_number int not null,
-                      team_name text not null,
-                      game_id int null ,
-                      constraint fk_team_game
-                          foreign key (game_id)
-                              references game(game_id)
-                              on delete set null
-                              on update cascade
+    team_id int primary key auto_increment,
+    team_token text not null,
+    team_number int not null,
+    team_name text not null,
+    game_id int null ,
+    constraint fk_team_game
+        foreign key (game_id)
+            references game(game_id)
+            on delete set null
+            on update cascade,
+    constraint uq_game_team_number
+                  unique (game_id, team_number)
+
 );
 
 create table response (
@@ -118,9 +121,20 @@ begin
            ('Title 2', 'Description 2', 1);
 
     INSERT INTO question (question_category, question_prompt, question_answer, question_type, question_round, question_order, theme_id)
-    VALUES ('category', 'question', 'answer', 'normal', 1, 1, 1),
+    VALUES ('category', 'prompt', 'answer', 'normal', 1, 1, 1),
            ('Category 2', 'Question 2', 'Answer 2', 'normal', 1, 2, 1),
            ('Category 1', 'Question 3', 'Answer 3', 'halftime', 1, 1, 2);
 
+    INSERT INTO game(game_code, host_token, game_status, current_round, current_question_id, theme_id)
+    values ('code', 'token', 'lobby', 1, 1, 1);
+
+    insert into team(team_token, team_number, team_name, game_id)
+    VALUES ('token', 1, 'name', 1),
+           ('token 2', 2, 'name 2', 1);
+
+
 end //
 delimiter ;
+
+
+select * from game;
