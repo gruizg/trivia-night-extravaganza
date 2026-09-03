2c4a54475f0fa52f6b522534ab9a35bf72de482c
Geshlee <geshlee.ruiz@gmail.com>
2026-09-03 04:22:19 -0400

    Implement team repository layer

--- server/src/main/java/learn.trivia/data/TeamRepository.java
+++ server/src/main/java/learn.trivia/data/TeamRepository.java
@@ -1,7 +1,15 @@
 package learn.trivia.data;
 
+import learn.trivia.models.Team;
+
+import java.util.List;
+
 public interface TeamRepository {
 
-    //TODO: IMPLEMENT
+    Team findById(int teamId);
+
+    List<Team> findAll();
+
+    Team add(Team team);
 
 }
--- server/src/test/java/learn/trivia/data/TeamJdbcClientRepositoryTest.java
+++ server/src/test/java/learn/trivia/data/TeamJdbcClientRepositoryTest.java
@@ -1,10 +1,16 @@
 package learn.trivia.data;
 
+import learn.trivia.models.Game;
+import learn.trivia.models.Team;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static learn.trivia.TestDataHelpers.Models.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -17,14 +23,50 @@ class TeamJdbcClientRepositoryTest {
     KnownGoodState knownGoodState;
 
     @BeforeEach
-    void setup() {
-        knownGoodState.set();
+    void setup() { knownGoodState.set(); }
+
+    @Nested
+    class Read {
+
+        @Test
+        void shouldFindById() {
+            Team team = repository.findById(1);
+
+            Game game = makeExistingGame();
+            assertNotNull(team);
+            assertEquals("token", team.getTeamToken());
+            assertEquals(1, team.getTeamNumber());
+            assertEquals("name", team.getTeamName());
+            assertEquals(makeExistingGame(), team.getGame());
+        }
+
+        @Test
+        void shouldFindAll() {
+            List<Team> teams = repository.findAll();
+
+            assertNotNull(teams);
+            assertEquals(2, teams.size());
+        }
     }
 
-    //TODO: IMPLEMENT TESTS
+    @Nested
+    class Create {
 
-    @Test
-    void check() {
+        @Test
+        void shouldAdd() {
+            Team toAdd = makeExistingTeam();
+            toAdd.setTeamId(0);
+            toAdd.setTeamNumber(3);
 
+            Team expected = makeExistingTeam();
+            expected.setTeamId(3);
+            expected.setTeamNumber(3);
+
+            Team actual = repository.add(toAdd);
+
+            assertNotNull(actual);
+            assertEquals(expected, actual);
+        }
     }
+
 }
