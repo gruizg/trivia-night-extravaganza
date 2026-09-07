30d5ee547d9fd20bbdd796aa284a65c05fcca86a
Geshlee <geshlee.ruiz@gmail.com>
2026-09-06 20:57:10 -0400

    Remove cors

--- server/src/main/java/learn.trivia/controllers/TeamController.java
+++ server/src/main/java/learn.trivia/controllers/TeamController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173"})
 @RequestMapping("/api/team")
 public class TeamController {
 
