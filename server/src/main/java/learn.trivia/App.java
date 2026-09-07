9288f626c6c002a7ea9d0344d5cd3e104d7b57a0
Geshlee <geshlee.ruiz@gmail.com>
2026-09-06 20:57:24 -0400

    Add cors config

--- server/src/main/java/learn.trivia/App.java
+++ server/src/main/java/learn.trivia/App.java
@@ -2,11 +2,24 @@ package learn.trivia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        }
     }
 }
