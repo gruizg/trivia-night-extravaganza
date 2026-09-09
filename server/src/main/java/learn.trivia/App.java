aa0da2f48b02970b1a9a66d177e6bc92b0af69fa
Geshlee <geshlee.ruiz@gmail.com>
2026-09-09 02:24:50 -0400

    Env variables for server origin

--- server/src/main/java/learn.trivia/App.java
+++ server/src/main/java/learn.trivia/App.java
@@ -1,5 +1,6 @@
 package learn.trivia;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +16,14 @@ public class App {
 
     @Configuration
     public class WebConfig implements WebMvcConfigurer {
+
+        @Value("${cors.allowed-origins}")
+        private String[] allowedOrigins;
+
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:3000",
-                            "http://10.0.0.80:3000")
+                    .allowedOrigins(allowedOrigins)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
         }
     }
